9ed493e7d12af16da1d7c78839675ef1d7bbbc1b
EliasMateu <eliasmateusmgoncalves@gmail.com>
2024-03-02 15:40:19 -0300

    Main: build - Excluir diretorio MenBottom, e criação do menuinferior

--- App.js
+++ App.js
@@ -1,18 +1,87 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
-import Home from './src/pages/Home';
-import Teste from "./src/pages/Teste";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Image } from 'react-native';
+import Home from './src/pages/Home/index';
+import Teste from './src/pages/Teste/index';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-export default function App() {
-  return(
+const MainTabScreen = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      headerShown: false,
+      activeTintColor: 'blue',
+      inactiveTintColor: 'gray',
+    }}
+  >
+    <Tab.Screen
+      name="Home"
+      component={Home}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color, size, focused }) => (
+          <Image 
+            source={require('./assets/Home.svg')}
+            style={{width: size, height: size, tintColor: focused ? 'green' : color}}
+          />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Teste"
+      component={Teste}
+      options={{
+        tabBarIcon: ({ color, size, focused }) => (
+          <Image 
+            source={require('./assets/Adicionar.svg')}
+            style={{width: size, height: size, tintColor: focused ? 'green' : color}}
+          />
+        ),
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Teste"
+      component={Teste}
+      options={{
+        tabBarIcon: ({ color, size, focused }) => (
+          <Image 
+            source={require('./assets/Adicionar.svg')}
+            style={{width: size, height: size, tintColor: focused ? 'green' : color}}
+          />
+        ),
+        headerShown: false,
+      }}
+    />
+  </Tab.Navigator>
+);
+
+const App = () => {
+  return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Teste" component={Teste} />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen 
+          name="Main" 
+          component={MainTabScreen} 
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name="Teste" 
+          component={Teste} 
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
